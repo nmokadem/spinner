110ebbe5d03b47e6d6154d22475fc4fb10364f7e
Nasser Mokadem <nmokadem@hotmail.com>
2021-11-03 00:06:39 +0000

    Add a new script to immitate a spinner

---
+++ spinner.js
@@ -0,0 +1,20 @@
+//process.stdout.write('hello from spinner1.js... \rheyyy\n');
+
+for (let i = 100; i < 10000; i += 900) {
+  setTimeout(() => {
+    process.stdout.write('\r|   ');
+  }, i);
+
+  setTimeout(() => {
+    process.stdout.write('\r/   ');
+  }, i + 200);
+
+  setTimeout(() => {
+    process.stdout.write('\r-   ');
+  }, i + 400);
+
+  setTimeout(() => {
+    // Need to escape the backslash since it's a special character.
+    process.stdout.write('\r\\   '); 
+  }, i + 600);
+}
